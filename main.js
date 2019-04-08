cbecf65b150634e406b636c7ff73acb443332cf8
Micka <baguet.mickael@gmail.com>
2019-04-08 16:03:02 +0200

    ajout isNaN

--- main.js
+++ main.js
@@ -2,13 +2,25 @@
 function calcul()
 {
   // Version détaillée
-  var pointure = document.getElementById("shoeSize").value;
-  var naissance = document.getElementById("yearOfBirth").value;
-  var res = ((((pointure*2)+5)*50)-naissance)+1766;
-  alert(pointure);
-  alert(naissance);
-  alert(res);
-  // Version courte
-  alert((((((document.getElementById("shoeSize").value)*2)+5)*50)-(document.getElementById("yearOfBirth").value))+1766)
+  alert(shoeSize.value);
+  alert(document.getElementById("yearOfBirth").value);
+  // Si les deux valeurs ne sont pas des chiffres :
+  if ((isNaN(shoeSize.value)) && (isNaN(document.getElementById("yearOfBirth").value)))
+  {
+    alert("Vous n'avez pas rentré des chiffres !");
+  }
+  else
+  {
+    var pointure = document.getElementById("shoeSize").value;
+    var naissance = document.getElementById("yearOfBirth").value;
+    var res = ((((pointure*2)+5)*50)-naissance)+1766;
+    alert(pointure);
+    alert(naissance);
+    alert(res);
+    // Version courte
+    alert((((((document.getElementById("shoeSize").value)*2)+5)*50)-(document.getElementById("yearOfBirth").value))+1766);
+
+  }
+
 
 }
