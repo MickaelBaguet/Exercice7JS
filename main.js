626d992be35c522804e9ffc4b5ed3063740fd40a
Micka <baguet.mickael@gmail.com>
2019-04-08 14:27:38 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// Fonction réalisant un calcul sur des valeurs
+function calcul()
+{
+  // Version détaillée
+  var pointure = document.getElementById("shoeSize").value;
+  var naissance = document.getElementById("yearOfBirth").value;
+  var res = ((((pointure*2)+5)*50)-naissance)+1766;
+  alert(pointure);
+  alert(naissance);
+  alert(res);
+  // Version courte
+  alert((((((document.getElementById("shoeSize").value)*2)+5)*50)-(document.getElementById("yearOfBirth").value))+1766)
+
+}
