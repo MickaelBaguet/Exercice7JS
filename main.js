9a016ce05663ce8e3ec5d0c0341cb27e03a02410
Micka <baguet.mickael@gmail.com>
2019-04-12 08:52:54 +0200

    correction

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
 // Fonction réalisant un calcul sur des valeurs
 function calcul()
 {
-  // Version détaillée
-  alert(shoeSize.value);
-  alert(document.getElementById("yearOfBirth").value);
-  // Si les deux valeurs ne sont pas des chiffres :
-  if ((isNaN(shoeSize.value)) && (isNaN(document.getElementById("yearOfBirth").value)))
+  var pointure = document.getElementById("shoeSize").value;
+  var naissance = document.getElementById("yearOfBirth").value;
+  // Si les deux valeurs sont des chiffres et qu'ils ne sont pas vide :
+  if (!isNaN(pointure) && !isNaN(naissance) && pointure && naissance)
   {
-    alert("Vous n'avez pas rentré des chiffres !");
+    var res = (pointure*2+5)*50-naissance+1766;
+    alert(res);
+    return res;
   }
   else
   {
-    var pointure = document.getElementById("shoeSize").value;
-    var naissance = document.getElementById("yearOfBirth").value;
-    var res = ((((pointure*2)+5)*50)-naissance)+1766;
-    alert(pointure);
-    alert(naissance);
-    alert(res);
-    // Version courte
-    alert((((((document.getElementById("shoeSize").value)*2)+5)*50)-(document.getElementById("yearOfBirth").value))+1766);
-
+    alert("Compléter correctement les champs !");
   }
+}
 
-
+document.getElementById('submit').onclick = function(){
+  alert(calcul());
 }
